566db029221ce19ff11d7c0eaea1532ad094f5ac
Venkat Kapisetti <venkat.kapisetti@gmail.com>
2023-03-27 06:55:55 -0400

    added commands

---
+++ kubelet-commsnds.sh
@@ -0,0 +1,229 @@
+ 1 kubectl get nodes
+   2 kubectl get nodes
+   3 kubectl get nodes
+   4 kubectl get nodes
+   5 kubectl get nodes
+   6 kubectl get nodes -o wide
+   7 az vm list -o table
+   8 az account set --subscription d8391944-774c-41db-95d3-6797158258bc
+   9 az aks get-credentials --resource-group az-trainings-rg --name myAKSCluster
+  10 az vm list -o table
+  11 kubectl get nodes
+  12 kubectl get nodes
+  13 kubectl get nodes -o wide
+  14 k get pods -n kube0system
+  15 k get pods -n kube-system
+  16 kubectl get pods -n kube-system
+  17 ls
+  18 cd .\Documents\azurebatch\
+  19 ls
+  20 cd .\docker-k8s\
+  21 ls
+  22 kubectl get pods
+  23 cat .\nginx.yml
+  24 kubectl apply -f .\nginx.yml
+  25 kubectl get pods
+  26 cat .\nginx.yml
+  27 kubectl get pods
+  28 helm repo add nginx-stable https://helm.nginx.com/stable
+  29 helm repo update
+  30 helm install my-release nginx-stable/nginx-ingress
+  31 kubectl get pods
+  32 kubectl get svc
+  33 kubectl get svc
+  34 k get ingrees -A
+  35 kubectl  get ingrees -A
+  36 kubectl  get ingres -A
+  37 kubectl  get ingress -A
+  38 kubectl  get ingress -A
+  39 kubectl get svc
+  40 kubectl get pods
+  41 kubectl get all
+  42 kubectl exec -it my-release-nginx-ingress-684c4589b8-7fdhl -- /bin/sh
+  43 cd ..
+  44 cd ..
+  45 git clone https://github.com/instana/robot-shop.git
+  46 kubectl get pods
+  47 cd .\azurebatch\
+  48 ls
+  49 cd .\docker-k8s\
+  50 ls
+  51 cd .\helm\
+  52 ls
+  53 cd .\templates\
+  54 ls
+  55 cd ..
+  56 kubectl create ns robot-shop
+  57 helm install robot-shop --namespace robot-shop .
+  58 alias k=kubectl
+  59 alias k = kubectl
+  60 alias
+  61 alias --help
+  62 alias help
+  63 alias  -h
+  64 get-help alias
+  65 new-alias -help
+  66 new-alias
+  67 k get pods
+  68 k get pods -n robot-shop
+  69 k get pods -n robot-shop  -o wide
+  70 k -n robot-shop describe pod/cart-f45c57b68-p2khr
+  71 k get pods -n robot-shop  -o wide
+  72 k get nodes -w
+  73 k get nodes
+  74 k get pods -n robot-shop  -o wide
+  75 k get pods -n robot-shop  -o wide
+  76 k get pods -n robot-shop  -o wide
+  77 k get pods -n robot-shop  -o wide
+  78 k get pods -n robot-shop  -o wide
+  79 k get pods -n robot-shop  -o wide
+  80 k get pods -n robot-shop  -o wide
+  81 k get pods -n robot-shop  -o wide
+  82 k -n robot-shop delete pod/cart-f45c57b68-p2khr
+  83 k get pods -n robot-shop
+  84 k get pods -n robot-shop -o wide
+  85 k get svc -n robot-shop
+  86 k get pods -n robot-shop -o wide
+  87 k -nrobot-shop describe pod/redis-0
+  88 k -nrobot-shop get pvc
+  89 k get sc
+  90 k get sc/azurefile
+  91 k get sc/azurefile -o yaml
+  92 k get sc/azurefile -o yaml  > standard.storage.class.yaml
+  93 ls
+  94 k -nrobot-shop get pvc
+  95 ls
+  96 cd ..
+  97 ls
+  98 cat .\standard.storage.class.yaml
+  99 k create -f .\standard.storage.class.yaml
+ 100 k get sc
+ 101 k get pods -n robot-shop
+ 102 k get pvc -A
+ 103 k get pvc -A
+ 104 k get pvc -A
+ 105 k get pods -n robot-shop
+ 106 k -n robop-shop delete --all pods
+ 107 k -n robot-shop delete --all pods
+ 108 k get pods -n robot-shop
+ 109 k get pods -n robot-shop
+ 110 k get pods -n robot-shop
+ 111 k get pods -n robot-shop
+ 112 k get pods -n robot-shop -w
+ 113 k get pods -n robot-shop
+ 114 k get pods -n robot-shop
+ 115 k get pods -n robot-shop
+ 116 k get pods -n robot-shop
+ 117 k get pods -n robot-shop
+ 118 k get pods -n robot-shop
+ 119 k get pods -n robot-shop -w
+ 120 k get pods -n robot-shop
+ 121 k -n robot-shop logs ratings-6d89976889-xx55x
+ 122 k -n robot-shop describe  ratings-6d89976889-xx55x
+ 123 k -n robot-shop describe pod/ratings-6d89976889-xx55x
+ 124 k -n robot-shop describe pod/ratings-6d89976889-xx55x
+ 125 k -n robot-shop describe pod/ratings-6d89976889-xx55x
+ 126 k -n robot-shop describe pod/ratings-6d89976889-xx55x
+ 127 k get pods -n robot-shop
+ 128 k get pods -n robot-shop
+ 129 k -n robot-shop delete pod/shipping-d9f58bbcf-d5v9p
+ 130 k get pods -n robot-shop
+ 131 k -n robot-shop delete pod/ratings-6d89976889-xx55x
+ 132 k get pods -n robot-shop
+ 133 k get pods -n robot-shop
+ 134 k get pods -n robot-shop
+ 135 k get pods -n robot-shop -w
+ 136 k -n robot-shop describe pod/shipping-d9f58bbcf-bkhvc
+ 137 k -n robot-shop get  pod/shipping-d9f58bbcf-bkhvc -o yaml
+ 138 k get ns
+ 139 k get pods
+ 140 k get pods -n robot-shop
+ 141 k get pods -n robot-shop -o wide
+ 142 k get svc  -n robot-shop -o wide
+ 143 k get all -A
+ 144 k get pods -n robot-shop
+ 145 k get deployments -n robot-shop
+ 146 k -n robot-shop scale deployment/user --replicas=2
+ 147 k get deployments -n robot-shop
+ 148 k get deployments -n robot-shop
+ 149 k get pods -n robot-shop
+ 150 k get deployments -n robot-shop
+ 151 k get sts -A
+ 152 k -n robot-shop get sts/redis -o yaml
+ 153 k get sts -A
+ 154 k get pvc -A
+ 155 k get ds -A
+ 156 k get svc -A
+ 157 k get svc -A
+ 158 helm ls
+ 159 helm ls -A
+ 160 helm uninstall -n robot-shop robot-shop
+ 161 k get svc -A
+ 162 k get svc -A
+ 163 k get svc -A
+ 164 k get svc -A
+ 165 k get svc -A
+ 166 k get pods -n robot-shop
+ 167 k get pods -n robot-shop
+ 168 k get pods -n robot-shop -w
+ 169 k get pods -n robot-shop
+ 170 k get all -n robot-shop
+ 171 k get svc -A
+ 172 helm ls -A
+ 173 helm uninstall my-release
+ 174 helm ls -A
+ 175 k get svc -A
+ 176 k get all
+ 177 k get all
+ 178 k get pods
+ 179 k get all
+ 180 k get all
+ 181 ls
+ 182 cd .\helm\
+ 183 ls
+ 184 helm install robot-shop --namespace robot-shop .
+ 185 k get pods -n robot-shop
+ 186 k get pods -n robot-shop
+ 187 k get pods -n robot-shop  -w
+ 188 k get pods -n robot-shop
+ 189 k get pods -n robot-shop
+ 190 k get pods -n robot-shop -w
+ 191 k get pods -n robot-shop -w
+ 192 k get pods -n robot-shop
+ 193 k get pods -n robot-shop
+ 194 k get pods -n robot-shop
+ 195 k get pods -n robot-shop
+ 196 k get pods -n robot-shop
+ 197 k get pods -n robot-shop -w
+ 198 k get pods -n robot-shop -w
+ 199 k get pods -n robot-shop -w
+ 200 k get pods -n robot-shop -w
+ 201 k -n robot-shop logs ratings-6d89976889-dshdm
+ 202 k -n robot-shop descrobe  ratings-6d89976889-dshdm
+ 203 k -n robot-shop describe  ratings-6d89976889-dshdm
+ 204 k -n robot-shop describe  pod/ratings-6d89976889-dshdm
+ 205 k get pods -n robot-shop -w
+ 206 k get pods -n robot-shop
+ 207 k -n robot-shop delete pod/ratings-6d89976889-dshdm
+ 208 k -n robot-shop delete pod/shipping-d9f58bbcf-k4vb8
+ 209 k get pods -n robot-shop
+ 210 k get pods -n robot-shop
+ 211 k get pods -n robot-shop
+ 212 k get pods -n robot-shop
+ 213 k get pods -n robot-shop
+ 214 k get pods -n robot-shop
+ 215 k get pods -n robot-shop
+ 216 k get pods -n robot-shop
+ 217 k get pods -n robot-shop
+ 218 k get pods -n robot-shop -w
+ 219 k get pods -n robot-shop
+ 220 k get svc  -n robot-shop
+ 221 cd ..
+ 222 ls
+ 223 helm
+ 224 helm create lokesh
+ 225 cd .\lokesh\
+ 226 ls
+ 227 helm install lokesh .
+ 228 k get svc
+ 229 k get pods
